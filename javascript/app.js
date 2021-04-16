eb8414bc9bb0dad03e9ff82897c0ff1d2046423d
juanpa022 <juanpa_galin@hotmail.com>
2021-04-16 20:20:46 -0300

    new

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,17 @@
 // Param numbers: array of numbers
 // Returns a sorted array
-function insertion_sort(numbers) {
+function insertion_sort(int[] numbers) {
 
     //Write your code here
+    int b,temp;
+    for(int i=1;i<numbers.size();i++)
+    {
+        temp=numbers[i];
+        for(int j=i-1;&j>=0 && numbers[j]>temp;j--){
+            numbers[j+1]=numbers[j];
+            numbers[j]=temp;
+        }
+    }
 
     return [-1];
 }
